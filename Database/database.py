fb767b4f5ddbcaa0b54f7390c730433e4b6c16f9
Ngahjordan <119456001+Ngahjordan@users.noreply.github.com>
2024-01-10 00:42:17 +0100

    Update database.py

--- Database/database.py
+++ Database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql://root:test123$@localhost/transport', echo=False)
+engine = create_engine('mysql://root:test50823$@localhost/transport', echo=False)
 
 Session = sessionmaker(bind=engine)
 session = Session()
